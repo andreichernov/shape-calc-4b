91ab94f385a2113582580b850ddbf5de484126ec
elicorrales <e_corrales2003@yahoo.com>
2016-08-27 14:15:44 -0400

    First Iteration - A command-line Application - Part 4

---
+++ shape-calc/src/main/java/com/eli/calc/shape/impl/ShapeCalculationsFactoryImpl.java
@@ -0,0 +1,10 @@
+package com.eli.calc.shape.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.eli.calc.shape.ShapeCalculationsFactory;
+
+@Component
+public class ShapeCalculationsFactoryImpl implements ShapeCalculationsFactory {
+
+}
